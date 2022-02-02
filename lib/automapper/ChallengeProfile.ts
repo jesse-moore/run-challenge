e2a1f3315c1a4a4c5c5f1dad99df168d8e20e58f
Jesse Moore <moore.jesse@gmail.com>
2022-02-02 07:14:34 -0600

    Update

--- lib/automapper/ChallengeProfile.ts
+++ lib/automapper/ChallengeProfile.ts
@@ -1,27 +1,22 @@
-import { CamelCaseNamingConvention, mapFrom, MappingProfile } from '@automapper/core'
+import { CamelCaseNamingConvention, mapFrom, MappingProfile, mapWith, mapWithArguments } from '@automapper/core'
 import { ChallengeDto, ChallengeIntervalDto } from '../../dtos/ChallengeDto'
 import { ChallengeFormDto, ChallengeIntervalFormDto } from '../../dtos/ChallengeFormDto'
+import { date } from '../../utils'
 
 export const challengeProfile: MappingProfile = (mapper) => {
-    mapper.createMap(ChallengeDto, ChallengeFormDto, {
-        namingConventions: {
-            source: new CamelCaseNamingConvention(),
-            destination: new CamelCaseNamingConvention(),
-        },
-    })
-    mapper.createMap(ChallengeFormDto, ChallengeDto, {
-        namingConventions: {
-            source: new CamelCaseNamingConvention(),
-            destination: new CamelCaseNamingConvention(),
-        },
-    })
-    mapper
-        .createMap(ChallengeIntervalFormDto, ChallengeIntervalDto, {
-            namingConventions: {
-                source: new CamelCaseNamingConvention(),
-                destination: new CamelCaseNamingConvention(),
-            },
+    mapper.createMap(ChallengeDto, ChallengeFormDto, {})
+    mapper.createMap(ChallengeFormDto, ChallengeDto, {}).forMember(
+        (destination) => destination.intervals,
+        mapFrom((source) => {
+            return source.intervals.map((interval) => {
+                return mapper.map(interval, ChallengeIntervalDto, ChallengeIntervalFormDto, {
+                    extraArguments: { start: source.start, interval: interval.interval },
+                })
+            })
         })
+    )
+    mapper
+        .createMap(ChallengeIntervalFormDto, ChallengeIntervalDto, {})
         .forMember(
             (destination) => destination.requirements,
             mapFrom((source) => {
@@ -32,10 +27,17 @@ export const challengeProfile: MappingProfile = (mapper) => {
                 return requirements
             })
         )
-    mapper.createMap(ChallengeIntervalDto, ChallengeIntervalFormDto, {
-        namingConventions: {
-            source: new CamelCaseNamingConvention(),
-            destination: new CamelCaseNamingConvention(),
-        },
-    })
+        .forMember(
+            (destination) => destination.start,
+            mapWithArguments((source, { start, interval }: { start: string; interval: number }) => {
+                return date.addDay(start, interval - 1)
+            })
+        )
+        .forMember(
+            (destination) => destination.end,
+            mapWithArguments((source, { start, interval }: { start: string; interval: number }) => {
+                return date.addDay(start + 'T23:59:59', interval - 1)
+            })
+        )
+    mapper.createMap(ChallengeIntervalDto, ChallengeIntervalFormDto, {})
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+dayjs.extend(duration)
+
 export const convert = {
     kmToMi: (km: number, mi: number): number => {
         return km * 0.621
@@ -10,5 +14,11 @@ export const convert = {
     },
     ftToM: (ft: number, m: number): number => {
         return ft * 0.305
+    },
+}
+
+export const date = {
+    addDay: (date: string, days: number) => {
+        return dayjs(date).add(days, 'day').format('YYYY-MM-DDTHH:mm:ss')
     },
 }
